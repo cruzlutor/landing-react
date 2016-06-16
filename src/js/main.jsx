998a4f254ef2362379db5a6942bc78af12a9f3c4
cruzlutor <cruz.torrez@gmail.com>
2016-06-16 10:51:04 -0500

    enable gh-pages

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -14,7 +14,7 @@ class Main extends React.Component{
     render(){
         return (
             <Router history={history} >
-                <Route path="src/" component={App} >
+                <Route path="/" component={App} >
                     <IndexRoute component={Home} />
                     <Route path="/search" component={Search} />
                     <Route path="/signin" component={Signin} />
--- src/js/utils/history.js
+++ src/js/utils/history.js
@@ -1,2 +1,4 @@
 import createBrowserHistory from 'history/lib/createBrowserHistory'
-export default createBrowserHistory()
+export default createBrowserHistory({
+    basename: '/landing-react/src/',
+})
